86e0c184234598f500b2b47a980d9fa92a09b4af
aarsh <aarsh.jp@gmail.com>
2015-10-24 06:47:52 -0400

    Put user data in mongodb

--- twit_test/get_user_data.py
+++ twit_test/get_user_data.py
@@ -1,12 +1,23 @@
 from ConfigParser import SafeConfigParser
 import time
+from pymongo import MongoClient
 import requests
+import json
 import tweepy
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
 requests.packages.urllib3.disable_warnings()
+
+#PyMonogo Data
+try:
+	client = MongoClient('localhost', 27017) # create a mongoclient
+	db = client['twitter_db']
+	collection = db['tweet_collection']
+	print("Connected Successfully")
+except pymongo.errors.ConnectionFailure, e:
+	print "Could not connect to monogodb: %s" % e
 
 parser = SafeConfigParser()
 parser.read('config.ini');
@@ -35,17 +46,39 @@ def get_tweets_from_user(username):
 	return user
 
 def get_homeline_tweets(username):
+	""" Gets the home timeline tweets for a given user """
 	friends = get_friends(username)
-	with open('tweets.json', 'a') as tweet_file:
-		for u in friends:
-			try:
-				tweet = get_tweets_from_user(u.screen_name)
-				tweet_file.write(str(tweet))
-				time.sleep(2)
-			except tweepy.TweepError:
-				time.sleep(2)
-				continue
+	for friend in friends:
+		try:
+			tweets = get_tweets_from_user(friend.screen_name)
+			for tweet in tweets:
+				collection.insert(tweet._json)
+			time.sleep(2)
+		except tweepy.TweepError:
+			time.sleep(2)
+			continue
+
+espn = get_tweets_from_user("espn")
+with open("espn_tweets.txt", 'a') as tweet_file:
+	for tweet in espn:
+		tweet_file.write(tweet.text.encode('utf-8'))
+
+# def test_pymongo(username):
+# 	friends = get_friends(username)
+# 	for friend in friends[0:1]:
+# 		try:
+# 			tweets = get_tweets_from_user(friend.screen_name)
+# 			for tweet in tweets[0:1]:
+# 				#print(tweet)
+# 				#tweet = json.loads(tweet)
+# 				collection.insert(tweet._json)
+# 				time.sleep(2)
+# 		except tweepy.TweepError:
+# 			time.sleep(2)
+# 			continue
 
 
-get_homeline_tweets("AarshPatel")
+#get_homeline_tweets("AarshPatel")
 
+#for tweet in collection.find():
+# 	print(tweet)
